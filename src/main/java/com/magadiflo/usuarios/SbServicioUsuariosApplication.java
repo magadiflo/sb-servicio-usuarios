2405f60b30449a4014b10d7c4f3a60f7b97f113d
Martín <magadiflo@gmail.com>
2022-09-05 19:35:48 -0500

    Inicio del proyecto Servicio usuarios

---
+++ src/main/java/com/magadiflo/usuarios/SbServicioUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbServicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbServicioUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magadiflo/usuarios/SbServicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbServicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
