062cc5ad5b83e8afea0409999aa13fc145281d67
Martín <magadiflo@gmail.com>
2022-09-20 17:34:11 -0500

    Exportar métodos repositorio rest con endpoint search

--- src/main/java/com/magadiflo/usuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/magadiflo/usuarios/models/dao/UsuarioDao.java
@@ -2,7 +2,9 @@ package com.magadiflo.usuarios.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.magadiflo.usuarios.models.entity.Usuario;
 
@@ -11,9 +13,22 @@ import com.magadiflo.usuarios.models.entity.Usuario;
 // Todo se exporta a nuestra ApiRest de forma automática
 @RepositoryRestResource(path = "usuarios") 
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
-
-	Usuario findByUsername(String username);
 	
+	/**
+	 * El path que se usa a partir de estos métodos incluirá siempre el /search
+	 * Ejemplo:
+	 * http://127.0.0.1:8090/api/usuarios/usuarios/search/buscar-username?username=magadiflo
+	 */
+
+	@RestResource(path = "buscar-username")
+	Usuario findByUsername(@Param("username") String username);
+	
+	/***
+	 * Este método como no se usó la anotación @RestResource como en el método anterior,
+	 * por defecto usará el nombre tal cual del método: obtenerPorUsername, para formar
+	 * parte de la URL. Pero tambíen se podría personalizar como en el método anterior
+	 * usando la anotación @RestResource
+	 */
 	@Query("SELECT u FROM Usuario AS u WHERE u.username = ?1")
 	Usuario obtenerPorUsername(String username);
 
