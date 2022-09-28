fca2941da1150cb6506d74996068bcd674afa8de
Martín <magadiflo@gmail.com>
2022-09-28 18:18:09 -0500

    Instalando PostgreSQL y creando la Base de datos en pgAdmin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES('magadiflo', '$2a$10$kdsfw.v.2wg2/xJ6Bq52d./yF8MOas4RALW43rGH4gM2K5mbjVPaS', 1, 'Martín', 'Díaz', 'magadiflo@magadiflo.com');
-INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES('admin', '$2a$10$932tMLvG8FkadCYyQstQE.f56WTfTENrlNty63sRkubDU/GrXNS9u', 1, 'Admin', 'Admin', 'admin@magadiflo.com');
+INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES('magadiflo', '$2a$10$kdsfw.v.2wg2/xJ6Bq52d./yF8MOas4RALW43rGH4gM2K5mbjVPaS', true, 'Martín', 'Díaz', 'magadiflo@magadiflo.com');
+INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES('admin', '$2a$10$932tMLvG8FkadCYyQstQE.f56WTfTENrlNty63sRkubDU/GrXNS9u', true, 'Admin', 'Admin', 'admin@magadiflo.com');
 
 INSERT INTO roles(nombre) VALUES('ROLE_USER'); 
 INSERT INTO roles(nombre) VALUES('ROLE_ADMIN');
